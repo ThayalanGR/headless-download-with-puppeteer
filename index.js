900ee878b4369a1214084bd93abc8867c87f2652
ThayalanGR <grthayalan18@gmail.com>
2020-04-01 13:46:38 +0530

    headless puppeteer file download express wrap up integration done

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
-const url = "https://www.ccilindia.com/FPIHome.aspx";
-const buttonToClickId = "#btnFPISWH";
-
 const puppeteer = require('puppeteer');
 const path = require('path');
 const express = require("express");
+const fs = require('fs');
+const mime = require('mime');
 
+
+const url = "https://www.ccilindia.com/FPIHome.aspx";
+const buttonToClickId = "#btnFPISWH";
+const downloadDirectory = path.resolve(__dirname, 'downloaded');
 const app = express();
+const PORT = process.env.PORT || 2000;
+
+app.get("/downloadfile", async (req, res) => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+        });
+        const page = await browser.newPage();
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: downloadDirectory
+        })
+        await page.goto(url);
+        await page.click(buttonToClickId);
+        await page.waitFor(1000);
+        await browser.close();
+        const fileToSend = fs.readdirSync(downloadDirectory)[0];
+        let file = fileToSend;
+        let filename = path.basename(file);
+        file = path.resolve(downloadDirectory, filename);
+        const mimetype = mime.getType(file);
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.setHeader('Content-type', mimetype);
+        const filestream = fs.createReadStream(file);
+        filestream.pipe(res);
+        setTimeout(() => {
+            clearDownloadDirectoryAsynchronusly();
+        }, 10000);
+    } catch (error) {
+        res.send({
+            status: false,
+            message: error.message
+        })
+    }
+
+})
 
 
-// app.get("/downloadfile", async (req, res) => {
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-    });
+app.listen(PORT, () => console.log("Headless Puppeteer file download server up and running on Port - ", +PORT));
 
-    const page = await browser.newPage();
-    await page._client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        downloadPath: path.resolve(__dirname, 'downloaded')
-    })
-    await page.goto(url);
-    console.log("Clicking Download Button...");
-    await page.click(buttonToClickId);
-    await page.waitFor(5000);
-    await browser.close();
-})();
 
-// })
+
+const clearDownloadDirectoryAsynchronusly = async () => {
+    try {
+        fs.readdir(downloadDirectory, (err, files) => {
+            if (err) throw err;
+            for (const file of files) {
+                fs.unlink(path.join(downloadDirectory, file), err => {
+                    if (err) throw err;
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error.message)
+    }
+}
